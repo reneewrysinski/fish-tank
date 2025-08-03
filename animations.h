36dd6d4f65d4323855b981a2773e94a0bec662bd
reneewrysinski <reneewrysinski@gmail.com>
2025-08-02 20:43:09 -0700

    added statics back (removing them broke it)

--- animations.h
+++ animations.h
@@ -5,7 +5,7 @@
 #define animations_h
 #endif
 
-const uint8_t* const FISH_SLEEP_FRAMES[] = {
+static const uint8_t* const FISH_SLEEP_FRAMES[] = {
     FISH_SLEEP_0, FISH_SLEEP_1, FISH_SLEEP_2, FISH_SLEEP_3, FISH_SLEEP_4,
     FISH_SLEEP_5, FISH_SLEEP_6, FISH_SLEEP_7, FISH_SLEEP_8, FISH_SLEEP_9,
     FISH_SLEEP_10, FISH_SLEEP_11, FISH_SLEEP_12, FISH_SLEEP_13, FISH_SLEEP_14,
@@ -13,24 +13,24 @@ const uint8_t* const FISH_SLEEP_FRAMES[] = {
     FISH_SLEEP_20, FISH_SLEEP_21, FISH_SLEEP_22
 };
 
-const uint8_t* const FISH_SLEEPING_FRAMES[] = {
+static const uint8_t* const FISH_SLEEPING_FRAMES[] = {
     FISH_SLEEP_19, FISH_SLEEP_20, FISH_SLEEP_21, FISH_SLEEP_22
 };
 
-const uint8_t* const FISH_SWIM_FRAMES[] = {
+static const uint8_t* const FISH_SWIM_FRAMES[] = {
     FISH_SWIM_0, FISH_SWIM_1, FISH_SWIM_2, FISH_SWIM_3, FISH_SWIM_4,
     FISH_SWIM_5, FISH_SWIM_6, FISH_SWIM_7, FISH_SWIM_8, FISH_SWIM_9,
     FISH_SWIM_10, FISH_SWIM_11, FISH_SWIM_12, FISH_SWIM_13, FISH_SWIM_14,
     FISH_SWIM_15, FISH_SWIM_16, FISH_SWIM_17, FISH_SWIM_18, FISH_SWIM_19
 };
 
-const uint8_t* const FISH_FEED_FRAMES[] = {
+static const uint8_t* const FISH_FEED_FRAMES[] = {
     FISH_FEED_0, FISH_FEED_1, FISH_FEED_2, FISH_FEED_3, FISH_FEED_4,
     FISH_FEED_5, FISH_FEED_6, FISH_FEED_7, FISH_FEED_8, FISH_FEED_9,
     FISH_FEED_10, FISH_FEED_11, FISH_FEED_12
 };
 
-const uint8_t* const FISH_GAME_FRAMES[] = {
+static const uint8_t* const FISH_GAME_FRAMES[] = {
     FISH_GAME_0, FISH_GAME_1, FISH_GAME_2, FISH_GAME_3, FISH_GAME_4,
     FISH_GAME_5, FISH_GAME_6, FISH_GAME_7, FISH_GAME_8, FISH_GAME_9,
     FISH_GAME_10, FISH_GAME_11, FISH_GAME_12, FISH_GAME_13, FISH_GAME_14,
@@ -38,51 +38,51 @@ const uint8_t* const FISH_GAME_FRAMES[] = {
     FISH_GAME_20, FISH_GAME_21, FISH_GAME_22
 };
 
-const uint8_t* const GAME_INTRO[] = {
+static const uint8_t* const GAME_INTRO[] = {
     FISH_GAME_0, FISH_GAME_1
 };
 
-const uint8_t* const GAME_HIDE_Y[] = {
+static const uint8_t* const GAME_HIDE_Y[] = {
     FISH_GAME_2
 };
 
-const uint8_t* const GAME_HIDE_P[] = {
+static const uint8_t* const GAME_HIDE_P[] = {
     FISH_GAME_3
 };
 
-const uint8_t* const GAME_HIDE_G[] = {
+static const uint8_t* const GAME_HIDE_G[] = {
     FISH_GAME_4
 };
 
-const uint8_t* const GAME_HIDE_R[] = {
+static const uint8_t* const GAME_HIDE_R[] = {
     FISH_GAME_5
 };
 
-const uint8_t* const GAME_WAVE[] = {
+static const uint8_t* const GAME_WAVE[] = {
     FISH_GAME_6, FISH_GAME_7, FISH_GAME_8
 };
 
-const uint8_t* const GAME_P_WIN[] = {
+static const uint8_t* const GAME_P_WIN[] = {
     FISH_GAME_10, FISH_GAME_11, FISH_GAME_12
 };
 
-const uint8_t* const GAME_G_WIN[] = {
+static const uint8_t* const GAME_G_WIN[] = {
     FISH_GAME_13, FISH_GAME_14, FISH_GAME_15
 };
 
-const uint8_t* const GAME_R_WIN[] = {
+static const uint8_t* const GAME_R_WIN[] = {
     FISH_GAME_8, FISH_GAME_16, FISH_GAME_17
 };
 
-const uint8_t* const GAME_Y_WIN[] = {
+static const uint8_t* const GAME_Y_WIN[] = {
     FISH_GAME_8, FISH_GAME_18, FISH_GAME_19
 };
 
-const uint8_t* const GAME_WIN[] = {
+static const uint8_t* const GAME_WIN[] = {
     FISH_GAME_20
 };
 
-const uint8_t* const GAME_LOSE[] = {
+static const uint8_t* const GAME_LOSE[] = {
     FISH_GAME_21, FISH_GAME_22
 };
 
